d2053405d14436b43441d9433f4a819922b8a5a8
gvdasolutions <gabranches@gmail.com>
2015-12-03 16:37:01 -0500

    added map

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 var server = app.listen(process.env.PORT || 5000);
 var path = require('path');
 global.appRoot = path.resolve(__dirname);
+var Map = require('./models/model');
 
 //-- App Config --//
 
@@ -16,6 +17,19 @@ app.set('view engine', 'jade');
 // Front page
 app.get('/', function (request, response) {
     response.render('pages/index');
+});
+
+// Map page
+app.get('/maps/:map', function (request, response) {
+	var map = request.params.map.toLowerCase();
+
+	// Get map data and render page
+	Map.find({tag: map}, function (err, doc) {
+	    if (err) throw err;
+	    response.render('pages/map', {map: doc});
+	});
+
+
 });
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,123 +1,23 @@
-var main = (function () {
+$(document).ready(function () {
 
-	me = {};
+});
 
-	me.game = '';
+//** Globals **//
 
-	function setBackground () {
-		var bg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+state = {
+	addNade: false
+}
 
-		$('body').css('background', 'url(/images/backgrounds/'+bg+'.jpg) no-repeat center center fixed');
-		$('body').css('-webkit-background-size', 'cover');
-		$('body').css('-moz-background-size', 'cover');
-		$('body').css('-o-background-size', 'cover');
-		$('body').css('background-size', 'cover');
+//** Events **//
+
+// Click on map squares
+$('.map-box').on('click', function () {
+
+	var boxNum = $(this).attr('box-num');
+
+	if (state.addNade) {
+		// Add nade
+	} else {
+		// Show nades
 	}
-
-	function loadHeroes () {
-		heroes.forEach(function (hero) {
-			
-			var heroHtml = '<div class="hero-div"></div>'
-			$('#hero-pick').append();
-
-		});
-	}
-
-	me.pickGame = function () {
-		return Math.floor(Math.random() * 3);
-	}
-
-	function loadGame (num) {
-		if (num === 0) {
-			socketHelper.emit('get-vs-data', client);
-		} else if (num === 1) {
-			socketHelper.emit('get-team-data', client);
-		} if (num >= 2) {
-			socketHelper.emit('get-solo-data', client);
-		} 
-
-	}
-
-	// Attaches a game to the front page div
-	me.attachGame = function (num) {
-		var num = num;
-
-			$('#loading-div').hide();
-			$('#loading-div-bottom').hide();
-			if (num === 0) {
-				$("#vs-game-div").show();
-			} else if (num === 1) {
-				$("#team-game-div").show();
-			} if (num >= 2) {
-				$("#solo-game-div").show();
-			} 
-	}
-
-	me.nextGame = function () {
-		$("#vs-game-div").hide();
-		$("#team-game-div").hide();
-		$("#solo-game-div").hide();
-		$('#loading-div p').html('Getting next match');
-		$('#loading-div').show();
-		$('#loading-div-bottom').show();
-
-		socketHelper.emit('status-request', client);
-
-		me.game = me.pickGame();
-		loadGame(me.game);
-	}
-
-
-	function populateDropdowns() {
-
-		// Sort heroes array
-		heroesSorted = heroes.slice();
-		heroesSorted.sort(function(a, b) {
-		    var textA = a.tag.toUpperCase();
-		    var textB = b.tag.toUpperCase();
-		    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-		});
-
-		heroesSorted.forEach(function(hero) {
-			$(".hero-dropdown").append('<option value="'+hero.id+'">'+hero.name+'</option>');
-		});
-
-
-		// Sort maps array
-		mapsSorted = maps.slice();
-		mapsSorted.sort(function(a, b) {
-		    var textA = a.tag.toUpperCase();
-		    var textB = b.tag.toUpperCase();
-		    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-		});
-
-		mapsSorted.forEach(function(map) {
-			$(".map-dropdown").append('<option value="'+map.id+'">'+map.name+ ' (' + map.strat + ')</option>');
-		});
-
-	}
-
-	$("#title").click(function() {
-		window.location = '/';
-	});
-
-	$(document).ready(function () {
-		setBackground();
-		me.nextGame();
-		populateDropdowns();
-
-	});
-
-	return me;
-
-}());
-
-
-// Google analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-58991641-3', 'auto');
-  ga('send', 'pageview');
+});
