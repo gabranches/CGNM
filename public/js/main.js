51ec8416d309ba5950de68ca85be5014ba2962af
gvdasolutions <gabranches@gmail.com>
2015-12-03 17:39:53 -0500

    added nade modal

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,23 @@ $('.map-box').on('click', function () {
 
 	if (state.addNade) {
 		// Add nade
+		$('#add-nade').modal('show');
 	} else {
 		// Show nades
+
 	}
+});
+
+
+// Add nade button
+$('#add-nade-button').on('click', function () {
+	state.addNade = true;
+	$(this).text("Select a spot on the map");
+});
+
+
+// Add nade modal close
+$('#add-nade').on('hidden.bs.modal', function () {
+	state.addNade = false;
+	$('#add-nade-button').text("Add a nade");
 });
