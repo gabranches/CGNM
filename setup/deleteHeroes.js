5dd7f9f0ba195943dc9b24d755ba1c1ba968d0f5
gvdasolutions <gabranches@gmail.com>
2015-12-14 22:38:13 -0500

    pre-launch

--- setup/deleteHeroes.js
+++
@@ -1,8 +0,0 @@
-var Heroes = require('../models/model.js').Maps;
-var heroes = require('./heroes.js');
-
-Heroes.remove({}, function(err) { 
-   console.log('collection removed');
-   process.exit()
-});
-
